ca9d0fab82aea9565ebb570eb1ac91998dbb431e
Gopi474 <168507830+Gopi474@users.noreply.github.com>
2024-05-18 11:07:59 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -29,7 +29,7 @@ bot = Client(
     bot_token=bot_token)
 
 
-@bot.on_message(filters.command(["Gopi"]))
+@bot.on_message(filters.command(["Gopitxt_bot"]))
 async def account_login(bot: Client, m: Message):
     editable = await m.reply_text("Hi!\n\nGive /Gopi Command to Downlaod From a Text file.\n")
 
